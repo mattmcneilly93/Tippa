18e32225634839de7dce4e82e4dae122ee898bb8
William Sæther <william.saether03@gmail.com>
2026-05-23 19:54:15 +0200

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { AppShell } from "@/components/app-shell";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: {
@@ -32,6 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
+        <Analytics/>
         <AppShell>{children}</AppShell>
       </body>
     </html>
